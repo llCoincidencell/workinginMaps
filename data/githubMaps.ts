8c1021bfbf037c808b80ba301010ab6a5a4dbace
llCoincidencell <100151924+llCoincidencell@users.noreply.github.com>
2025-11-23 22:59:31 +0300

    Update githubMaps.ts

--- data/githubMaps.ts
+++ data/githubMaps.ts
@@ -13,7 +13,7 @@ const getUrl = (filename: string) => {
 // ÖNEMLİ: filename kısmı GitHub'daki dosya ismiyle BİREBİR (Büyük/Küçük harf duyarlı) aynı olmalıdır.
 export const availableMaps = [
   { name: 'BOKA Sınırları', filename: 'BOKA.kmz' },
-  { name: 'Ovalar', filename: 'ovalar.kmz' }, // Düzeltildi: Küçük harf ile ovalar.kmz
+  { name: 'Ovalar', filename: 'OVALAR.kmz' }, // Düzeltildi: Küçük harf ile ovalar.kmz
   { name: 'Su Tahsis Alanları', filename: 'SU_tahsis_alanlari.kmz' },
   { name: 'Tüm Korunan Alanlar', filename: 'tum_korunan_alanlar.kmz' }
 ].map(map => ({
