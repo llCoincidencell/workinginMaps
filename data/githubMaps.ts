ba11107748c63791c9b8c3e620e9680ff6c4dd7b
llCoincidencell <100151924+llCoincidencell@users.noreply.github.com>
2025-11-23 20:01:45 +0300

    Add raw link to githubMaps array

--- data/githubMaps.ts
+++ data/githubMaps.ts
@@ -1,7 +1,7 @@
 
 // Buraya GitHub'a yüklediğiniz dosyaların "Raw" linklerini ekleyin.
 // Örnek: 'https://raw.githubusercontent.com/kullaniciadi/reponame/main/harita.kmz'
-export const githubMaps = [
+export const githubMaps = [https://github.com/llCoincidencell/workinginMaps/raw/refs/heads/main/BOKA.kmz]
   // Şimdilik boş veya test amaçlı bir link bırakabilirsiniz.
   // Kendi linkinizi buraya eklediğinizde uygulama açılışta bunu yükleyecektir.
 ];
