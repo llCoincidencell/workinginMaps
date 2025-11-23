5bc7bf838afe63dd3013097bcf1e5855da440cd9
llCoincidencell <100151924+llCoincidencell@users.noreply.github.com>
2025-11-23 22:16:51 +0300

    Update filename for 'Su Tahsis Alanları' map

--- data/githubMaps.ts
+++ data/githubMaps.ts
@@ -15,7 +15,7 @@ export const availableMaps = [
   { name: 'BOKA Sınırları', filename: 'BOKA.kmz' },
   { name: 'Ovalar', filename: 'OVALAR (4).kmz' },
   { name: 'Ovalar', filename: 'OVALAR.kmz' },
-  { name: 'Su Tahsis Alanları', filename: 'SU TAHSİS ALANLARI (9).kmz' },
+  { name: 'Su Tahsis Alanları', filename: 'SU_tahsis_alanlari.kmz' },
   { name: 'Tüm Korunan Alanlar', filename: 'tum_korunan_alanlar.kmz' }
 ].map(map => ({
   name: map.name,
