a18a2969f7270243d1f7133785ebc3cfd8e0361f
llCoincidencell <100151924+llCoincidencell@users.noreply.github.com>
2025-12-02 15:00:54 +0300

    Update githubMaps.ts

--- data/githubMaps.ts
+++ data/githubMaps.ts
@@ -1,6 +1,4 @@
 
-import { OVALAR_DATA } from './embedded_ovalar';
-
 // GITHUB YAPILANDIRMASI
 export const USER: string = 'llCoincidencell'; 
 export const REPO: string = 'workinginMaps';      
@@ -14,29 +12,18 @@ const getGitHubUrl = (filename: string) => {
 // Harita Listesi
 // filename: GitHub'dan çeker
 // url: Harici linkten çeker
-// data: Doğrudan kodun içinden (Gömülü) çeker
 const mapsConfig = [
-  { name: 'Bursa Ovası (Gömülü)', data: OVALAR_DATA },
+  { name: 'bursa ova', filename: 'bursaova.kml' },
   { name: 'BOKA Sınırları', filename: 'BOKA.kmz' },
-  { name: 'Ovalar (GitHub)', filename: 'OVALAR (4).kmz' }, 
   { name: 'Su Tahsis Alanları', filename: 'SU TAHSİS ALANLARI (9).kmz' }, 
   { name: 'Tüm Korunan Alanlar', filename: 'tum_korunan_alanlar.kmz' }
 ];
 
 export const availableMaps = mapsConfig.map(map => {
-  // Eğer gömülü data varsa
-  if ('data' in map) {
-    return {
-      name: map.name,
-      url: null,
-      data: map.data
-    };
-  }
-  
   // URL veya Filename varsa
   return {
     name: map.name,
-    url: 'url' in map ? map.url : getGitHubUrl(map.filename as string),
+    url: 'url' in map ? (map as any).url : getGitHubUrl((map as any).filename),
     data: null
   };
 });
